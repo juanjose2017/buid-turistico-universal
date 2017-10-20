bdc3484a5bbaea8d6fd1d3155ac1377506132eb2
juanjose2017 <jjgamboa86@misena.edu.co>
2017-10-20 13:08:23 -0500

    ss

---
+++ main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".carousel").carousel(),$(".parallax").parallax(),$(".button-collapse").sideNav(),$(".slider").slider(),$(".button-collapse").sideNav(),$(".carousel.carousel-slider").carousel({fullWidth:!0})});
